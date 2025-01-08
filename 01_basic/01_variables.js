500cdf0f887d4cdf3f47564ff0a88a83e2f22b53
Himanshu8211 <162582571+Himanshu8211@users.noreply.github.com>
2025-01-08 17:11:36 +0000

    datatype

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,16 @@
+const accountId =1345443
+let accountEmail="dubeyhimanshu289@gmail.com"
+var accountPassword ="12345"
+accountCity ="jaipur"
+let accountState;
+
+//accountId =2//not allowed
+
+console.log(accountEmail);
+console.log(accountState);
+
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
+// if value is not decleared or given then it shows undefined
---
+++ 01_basic/02_datatype.js
@@ -0,0 +1,28 @@
+"use strict";// treat all js code as newer version
+//alert(3+3)//we are using node js, not browser
+
+// we avoid semicolon in react, js
+console.log(3
+    +3)// code readibility should high
+
+
+console.log("HImanshu")
+let name ="himanshu"
+let age = 19
+let isLoggedIn = false
+
+// number => 2 to power 53 (range in memmory address)
+// bigint is used for big number like in trading .
+// string=> ""or ''
+//boolean=> true/false
+// null=> standalone value(representation of empty value)
+
+// undefined => value not assigned
+// symbol => for uniqueness
+// these above are premmitive data type.
+
+// object
+
+console.log(typeof "himanshu");
+console.log(typeof age );
+console.log(typeof undefined);// undefined.
